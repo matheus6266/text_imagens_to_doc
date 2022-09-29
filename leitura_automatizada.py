f1f63bd745e1f5f0541a2e5c1fb7629d692355fe
Matheus Barros Oliveira <88863005+matheus6266@users.noreply.github.com>
2022-09-29 17:48:54 -0400

    Add files via upload
    reading from a model

--- leitura_automatizada.py
+++ leitura_automatizada.py
@@ -8,6 +8,7 @@ clear()
 # Caminho para o aplicativo do Tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Tesseract\\tesseract'
 
+dir_name_model = "C:\\TCC_data_science\\projeto_tio_otavio\\"
 total_imagens = int(input("Inform the number of images to be read: "))
 format_text = '.docx'
 
@@ -40,7 +41,11 @@ for i in range(total_imagens):
 	print("Reading de", i + 1 ,"° image")
 	leitura_texto = pytesseract.image_to_string(os.path.join(dir_name_images, base_filename_image + format_images), lang = 'por', config = '--psm 12')
 	print("Writting the", i + 1 ,"° image")
-	doc = docx.Document()
+
+	document_model = "MODELO 14X21"
+
+	doc = docx.Document(os.path.join(dir_name_model, document_model + format_text))
+	## doc = docx.Document() Creating a new file 
 	doc.add_paragraph("%s" % leitura_texto)
 	doc.save(os.path.join(dir_name_docs, base_filename_docs + format_text))
 	print("\n")
